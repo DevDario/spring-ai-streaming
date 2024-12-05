35f0f96f92a9e36f18de045b62762fb80eb5c4e3
Dário Silva <dariosilva13222@gmail.com>
2024-12-05 18:29:43 +0100

    initial commit

---
+++ src/main/java/ao/com/devdario/spring_ai_streaming/SpringAiStreamingApplication.java
@@ -0,0 +1,13 @@
+package ao.com.devdario.spring_ai_streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiStreamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ao/com/devdario/spring_ai_streaming/SpringAiStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.com.devdario.spring_ai_streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
