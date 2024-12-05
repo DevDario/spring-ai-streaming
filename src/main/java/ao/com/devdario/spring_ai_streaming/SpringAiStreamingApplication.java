e84627e2d1185e7a1696956d86a7f353a662eb60
Dário Silva <dariosilva13222@gmail.com>
2024-12-05 18:43:37 +0100

    build: adds support to env files

--- src/main/java/ao/com/devdario/spring_ai_streaming/SpringAiStreamingApplication.java
+++ src/main/java/ao/com/devdario/spring_ai_streaming/SpringAiStreamingApplication.java
@@ -2,8 +2,10 @@ package ao.com.devdario.spring_ai_streaming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:${user.dir}/.env")
 public class SpringAiStreamingApplication {
 
 	public static void main(String[] args) {
